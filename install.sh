ee204e0c73453a76977ea8386d07a8da2b5560bf
Cyrus Chan <cyruschan1212@gmail.com>
2021-03-16 10:43:10 +0800

    Rework on script

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# This shell script is running on WRT that setup all necessary tasks
+
---
+++ updateIndex.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# This is specific task on /etc/init.d which
+# ensure the content is modified when LuCI updated
+
+cp /root/custIdx/index.html /www/index.html
